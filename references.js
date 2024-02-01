561870822dd24402abccb48ee20c900a08ac8481
Jeyfrem <jeyfrem@proton.me>
2024-02-01 09:18:28 -0500

    Create references.js

---
+++ references.js
@@ -0,0 +1,36 @@
+//send out global message with
+
+alert(`example`);
+
+//calculate percentages with (using 10% as example)
+
+(10/100);
+
+//use `` for string interpolation. better than concatenation with '' or ""
+//example:
+
+`Estimated tax (${10}%): $${Math.round((799 * 2 + 2095 * 2 + 499 + 499) * (10/100))}`;
+
+
+
+//Math.round() is used to round numbers to the nearest value. 
+
+Math.round();
+
+//Math.floor() nears to the lowest value and Math.ceil() to the highest.
+
+Math.floor();
+
+Math.ceil();
+
+
+
+//you're building this, eventually. don't lose it. it calculates celsius and fahrenheit
+
+//need two values entered for either or.
+
+//fahrenheit
+(celsius * (9/5)) + 32
+
+//celsius
+(fahrenheit - 32) * (5/9)
