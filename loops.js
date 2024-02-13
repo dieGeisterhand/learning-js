d6000886a58c9c3bdcc2916e31eab6f8038d9e2c
Jeyfrem <jeyfrem@proton.me>
2024-02-13 14:58:02 -0500

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,6 +1,17 @@
-    // initialize loop // stoping condition // iteration statement
-    for (let counter = 0; counter < 4; counter++) {
-      console.log(counter);
-    }
+// initialize loop // stoping condition // iteration statement
 
-    
+// print 0 through 3
+for (let counter = 0; counter < 4; counter++) {
+  console.log(counter);
+}
+
+
+// print 5 through 10
+for (let i = 5; i < 11; i++) {
+  console.log(i);
+}
+
+// same thing but from 5 to 0
+for (let counter = 5; counter >= 0; counter--){
+  console.log(counter);
+}
