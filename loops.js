afd62282aced5309d27edf82bc07a7a972ce9e93
Jeyfrem <jeyfrem@proton.me>
2024-02-13 22:02:32 -0500

    Update loops.js

--- loops.js
+++ loops.js
@@ -4,3 +4,21 @@
     }
 
     
+// nested loops.
+
+// on example below, for each follower bob has, we iterate through every single one tina happens to have.
+    
+const bobsFollowers = [`carlos`, `andres`, `pedro`, `miguel`];
+
+const tinasFollowers = [`miguel`, `pedro`, `roberto`];
+
+const mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i])
+      console.log(mutualFollowers);
+    }
+  }
+}
