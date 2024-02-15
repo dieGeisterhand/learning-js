33adbc7dcb944ee0451a43df5552e8f478596ecc
Jeyfrem <jeyfrem@proton.me>
2024-02-15 10:30:13 -0500

    adding content

---
+++ iterators.js
@@ -0,0 +1,29 @@
+const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+
+
+// forEach()
+artists.forEach(artist => {
+  console.log(artist + ' is one of my favorite artists.');
+});
+
+
+
+
+const numbers = [1, 2, 3, 4, 5];
+
+// map()
+const squareNumbers = numbers.map(number => {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+// filter()
+const onlyNumbers = things.filter(thing => {
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
+
