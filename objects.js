af9e657aa740ca93a5c8de338ca80c66ad853420
Jeyfrem <jeyfrem@proton.me>
2024-02-19 10:56:47 -0500

    Update objects.js

--- objects.js
+++ objects.js
@@ -5,3 +5,11 @@ let spaceship = {
   color: 'silver'
 };
   //keys : value
+
+// object 'keys' can be accessed using dot notation, such as
+
+spaceship.color; // 'silver'
+
+// if keys have spaces or special character, they must be accessed using bracket notation
+
+spaceship['Fuel Type'];
