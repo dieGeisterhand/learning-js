4b3803429cdd36451376f62fd28aad9e3a4f9773
Jeyfrem <jeyfrem@proton.me>
2024-02-19 11:54:24 -0500

    Update objects.js

--- objects.js
+++ objects.js
@@ -70,3 +70,71 @@ let alienShip = {
 
 alienShip.retreat();
 alienShip.takeOff();
+
+
+
+const spaceship = {
+  telescope: {
+     yearBuilt: 2018,
+     model: '91031-XLT',
+     focalLength: 2032 
+  },
+  crew: {
+     captain: { 
+         name: 'Sandra', 
+         degree: 'Computer Engineering', 
+         encourageTeam() { console.log('We got this!') } 
+      }
+  },
+  engine: {
+     model: 'Nimbus2000'
+  },
+  nanoelectronics: {
+      computer: {
+         terabytes: 100,
+         monitors: 'HD'
+      },
+     'back-up': {
+        battery: 'Lithium',
+        terabytes: 50
+      }
+ }
+}; 
+
+
+spaceship.nanoelectronics['back-up'].battery; // Returns 'Lithium'
+
+
+
+let spaceship = {
+  passengers: [{name: 'dog'}],
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
+
+const capFave = spaceship.crew.captain['favorite foods'][0]; // 'cookies'
+
+const firstPassenger = spaceship.passengers[0]; // selecting name object
