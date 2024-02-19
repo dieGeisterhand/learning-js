f2ee50af0eae34eb31855248eaeb298e98354cdc
Jeyfrem <jeyfrem@proton.me>
2024-02-19 11:26:10 -0500

    Update objects.js

--- objects.js
+++ objects.js
@@ -13,3 +13,25 @@ spaceship.color; // 'silver'
 // if keys have spaces or special character, they must be accessed using bracket notation
 
 spaceship['Fuel Type'];
+
+// new properties can be added via dot notation
+
+spaceship.speed = '50ms';
+
+// they can also be deleted with the delete operator
+
+delete spaceship.speed;
+
+
+
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth',
+  color: 'silver',
+  'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+spaceship.color = 'glorious gold';
+spaceship.numEngines = 4;
+delete spaceship['Secret Mission'];
