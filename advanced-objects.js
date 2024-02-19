719b7d59d949b5e8fb0793643cf3214ebf2c1861
Jeyfrem <jeyfrem@proton.me>
2024-02-19 14:04:56 -0500

    Update advanced-objects.js

--- advanced-objects.js
+++ advanced-objects.js
@@ -10,3 +10,29 @@ const goat = {
 goat.makeSound(); // Prints baaa
 
 
+
+const goat = {
+  dietType: 'herbivore',
+  makeSound() {
+    console.log('baaa');
+  },
+  diet() {
+    console.log(dietType);
+  }
+};
+goat.diet(); 
+// Output will be "ReferenceError: dietType is not defined"
+// dietType is out of scope.
+
+const goat = {
+  dietType: 'herbivore',
+  makeSound() {
+    console.log('baaa');
+  },
+  diet() {
+    console.log(this.dietType);
+  }
+};
+
+goat.diet(); 
+// Output: herbivore
