d8f06c326fea43d8a3def9d8be296377804bb5c8
Jeyfrem <jeyfrem@proton.me>
2024-02-20 13:04:08 -0500

    add

--- arrow-function.js
+++ arrow-function.js
@@ -20,3 +20,12 @@ const squareNum = num => num * num;
 
 
 // https://discuss.codecademy.com/t/what-is-the-syntax-for-arrow-functions/438372
+
+
+function next(x) {
+  return x + 1;
+}
+
+const arr = [1, 2, 3];
+
+arr.map(next); // doonie's console shows [2, 3, 4]
--- loops.js
+++ loops.js
@@ -1,6 +1,5 @@
 // initialize loop // stoping condition // iteration statement
 
-
 // print 0 through 3
 for (let counter = 0; counter < 4; counter++) {
   console.log(counter);
@@ -15,43 +14,17 @@ for (let i = 5; i < 11; i++) {
 // same thing but from 5 to 0
 for (let counter = 5; counter >= 0; counter--){
   console.log(counter);
-
-    
-// nested loops.
-
-// on example below, for each follower bob has, we iterate through every single one tina happens to have.
-    
-const bobsFollowers = [`carlos`, `andres`, `pedro`, `miguel`];
-
-const tinasFollowers = [`miguel`, `pedro`, `roberto`];
-
-const mutualFollowers = [];
-
-for (let i = 0; i < bobsFollowers.length; i++) {
-  for (let j = 0; j < tinasFollowers.length; j++) {
-    if (bobsFollowers[i] === tinasFollowers[j]) {
-      mutualFollowers.push(bobsFollowers[i])
-      console.log(mutualFollowers);
-    }
-  }
 }
 
-
-
-
-
-
-
-for (let i = 1; i < 4; i++) {
-  console.log(i);
+// iterating through an array using a loop. output would be grizzly bear, sloth, sea lion
+const animals = ['Grizzly Bear', 'Sloth', 'Sea Lion'];
+for (let i = 0; i < animals.length; i++){
+  console.log(animals[i]);
 }
 
+// same thing but with a string
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
 
-
-// while loop. for loop that achieves the same result above for reference. notice the global variable established before.
-
-let i = 1;
-while (i < 4) {
-  console.log(i);
-  i++;
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log(`I would love to visit ${vacationSpots[i]}`);
 }
