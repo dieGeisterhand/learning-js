10e6b4278a7aa28c6b31a98d049479d05ad92b67
diegeisterhand <jeyfrem@proton.me>
2024-03-07 11:39:36 -0500

    triangle

---
+++ dragon-repeller/test.js
@@ -0,0 +1,12 @@
+
+// so I saw a meme and I wanted to give it a go. 
+// https://scontent-atl3-2.xx.fbcdn.net/v/t1.15752-9/431465364_3596852020575441_5801226062128378217_n.png?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=BIReaAa2K2AAX9U43VS&_nc_oc=AQks-W-YuLX2D2DPS__h-a1ffw6EXERK6QW4p_j_CgF7Jtcp4IADpzAQ2m8i3Dth-6s&_nc_ht=scontent-atl3-2.xx&oh=03_AdTM-tpyBFNmOfUo4nbVx-TqBA831dEhbvNDHJDePXGNnw&oe=66114AD1
+
+
+function triangle(size) {
+    for (let i = 0; i < size; i++) {
+        console.log("*".repeat(i));
+    }
+}
+
+triangle(6);
