80e55f1545c2b348162caabed5a49b44fdb352cd
jeyfrem <jeyfrem@proton.me>
2024-03-25 15:30:39 -0400

    quack

--- switch-case.js
+++ switch-case.js
@@ -15,3 +15,59 @@ function lifePhase(age) {
     return `senior citizen`;
   }
 }
+
+
+function finalGrade(homework, midterm, final) {
+  
+  if ((homework < 0 || homework > 100) || (midterm < 0 || midterm > 100) || (final < 0 || final > 100)) {
+    return `You have entered an invalid grade.`;
+  }
+  
+  const average = (homework + midterm + final) / 3;
+
+  switch (true) {
+    case (average <= 59):
+      return `F`;
+      break;
+    case (average <= 69):
+      return `D`;
+      break;
+    case (average <= 79):
+      return `C`;
+      break;
+    case (average <= 89):
+      return `B`;
+      break;
+    case (average <= 100):
+      return `A`;
+      break;
+  }
+}
+
+console.log(finalGrade(99, 92, 95)) // Should print 'A'
+
+
+function howOld(age, year) {
+  const currentYear = new Date().getFullYear();
+  const yearBorn = currentYear - age;
+
+  switch (true) {
+    case (year > currentYear):
+    return `You will be ${(year - currentYear) + age} in the year ${year}`;
+    break;
+
+    case (year < yearBorn):
+    return `The year ${year} was ${yearBorn - year} years before you were born`;
+    break;
+
+    case (year > yearBorn):
+    return `You were ${year - yearBorn} in the year ${year}`;
+    break;
+  }
+
+  return;
+
+}
+
+
+console.log(howOld(29, 1990));
